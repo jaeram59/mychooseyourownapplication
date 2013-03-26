f1a43e400fa8a92ccba4646ae94f329beb68006e
Bryan Taylor <jaeram59@gmail.com>
2013-03-26 15:28:28 -0400

    commit 10

--- scripts/question-viewmodel.js
+++ scripts/question-viewmodel.js
@@ -11,8 +11,43 @@ App.ViewModels.Question = function(questionData){
 		}, 0);
 	});
 
+	self.getExistingVote = function(voterName){
+		return _(self.votes()).find(function(vote){
+			return vote.voter === voterName;
+		});
+	}
+
+	self.userVoted = function(voteValue){
+		var userVoted = self.getExistingVote(overflowViewModel.userName());
+		var userVoted = (userVote && userVote.value === voteValue);
+		return userVoted;
+	}
+
+	self.removeExistingVote = function(existingVote){
+		self.votes.remove( function(vote){
+			return vote.voter === existingVote.voter;
+		});
+	}
+
+	self.userVotedUp = ko.computed( function(){
+		return self.userVoted(1);
+	});
+
+	self.userVotedDown = ko.computed( function(){
+		return self.userVoted(-1);
+	});
+
 	self.addVote = function(voter, voteType){
 		var voteValue = voteType === 'up' ? 1 : -1;
+		var existingVote = self.getExistingVote(voter);
+
+		if(existingVote){
+			if(existingVote.value !== voteValue){
+				self.removeExistingVote(existingVote);
+			}
+		}
+		else{
 		self.votes.push( {value: voteValue, voter: voter});
+		}
 	}
 }
